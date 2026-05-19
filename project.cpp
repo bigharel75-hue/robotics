ec3207d9e46a375e31deddaec5bbb47feea2af8c
bigharel75-hue <bigharel75@gmail.com>
2026-05-19 10:40:56 +0000

    Refactor project.cpp for clarity and comments

--- project.cpp
+++ project.cpp
@@ -2,7 +2,7 @@
 #define trigPin A4
 #define echoPin A3
 #define BAUD_RATE 115200
-#define black 0
+#define black 0 //no need of this
 #define white 1024
 #define motorL0 5
 #define motorL1 9
@@ -12,17 +12,17 @@
 #define tcrtL A1
 #define tcrtR A2
 
-motor motorL(motorL0, motorL1);
+motor motorL(motorL0, motorL1);//defines the motors
 motor motorR(motorR0, motorR1);
 int firstColor = 0;
 int secColor = 0;
 
 void setup() {
-  for (int i = 3; i <= 10; i++)
+  for (int i = 3; i <= 10; i++)//defines the motors to OUTPUT
   {
     pinMode(i, OUTPUT);
   }
-  pinMode(tcrtL, INPUT);
+  pinMode(tcrtL, INPUT);//no need of this, was for line folower
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   Serial.begin(BAUD_RATE);
@@ -38,8 +38,8 @@ void setup() {
 void loop() 
 {
   int distance = readUltraSonic();
-  int error = (distance-15)*CENTIMETERS_TO_DRIVE;
-  motorR.move(error);
+  int error = (distance-15)*CENTIMETERS_TO_DRIVE;//i used const, next time ill use map, easier
+  motorR.move(error);//moves the error
   motorL.move(error);
   //motorR.move((error < 0)?-100:error);
   //motorL.move((error < 0)?-100:error);
@@ -57,12 +57,14 @@ int readUltraSonic()
 {
   long duration = 0;
   int distance = 0;
-  digitalWrite(trigPin, LOW);
+  digitalWrite(trigPin, LOW);//resets the pin
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH);
-  distance = duration * 0.034 / 2;
+  duration = pulseIn(echoPin, HIGH);//gets the duration
+  distance = duration * 0.034 / 2;//calculates the distance
   return distance;
 }
+
+
